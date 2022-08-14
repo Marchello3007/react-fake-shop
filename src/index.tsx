11195a219fd36c6bb4513659f0a24d7fe7ca9c69
Oleg <marchello3007@gmail.com>
2022-08-14 09:13:22 +0300

    add react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,16 @@ const h1 = <h1>Hello, Hello World</h1>
 
 function Blabla() {
     return(
+        <>
         <div>
             {h1}
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore ratione quod placeat architecto quam! Nobis minima illum voluptas quos culpa magnam! Ullam molestiae cum consectetur accusamus. Nulla dolores nisi quo!</p>
+            <p>Lorem, 333 ipsum dolor sit amet consectetur adipisicing elit. Inventore ratione quod placeat architecto quam! Nobis minima illum voluptas quos culpa magnam! Ullam molestiae cum consectetur accusamus. Nulla dolores nisi quo!</p>
         </div>
+        <div>
+            {h1}
+            <p>Lorem, 333 ipsum dolor sit amet consectetur adipisicing elit. Inventore ratione quod placeat architecto quam! Nobis minima illum voluptas quos culpa magnam! Ullam molestiae cum consectetur accusamus. Nulla dolores nisi quo!</p>
+        </div>
+        </>
     )
 }
 
