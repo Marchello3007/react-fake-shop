b1f7cc9708b8ed74d316333359f51bff8525c8ee
Oleg <marchello3007@gmail.com>
2022-08-18 13:57:41 +0300

    add dynamic data to ProductListItem

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -8,7 +8,7 @@ const ProductList = (props: Props) => {
     return (
         <>
             <Typography variant="h4" textAlign="center" margin={3}>
-                Product List
+                Product List99
             </Typography>
             <Grid
                 container
@@ -18,22 +18,31 @@ const ProductList = (props: Props) => {
                 spacing={3}
             >
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone X"
+                        description="This is iPhone X"
+                        type="phone"
+                        capacity={64}
+                        price={500}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone XS"
+                        description="This is iPhone XS"
+                        type="phone"
+                        capacity={32}
+                        price={600}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem
+                        name="iPhone 13 Pro"
+                        description="This is iPhone 13 Pro"
+                        type="phone"
+                        capacity={124}
+                        price={900}
+                    />
                 </Grid>
             </Grid>
         </>
