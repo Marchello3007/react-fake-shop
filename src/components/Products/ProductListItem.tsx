6dad4d616eeed87074bfc95f7d6af65dcdbc3472
Oleg <marchello3007@gmail.com>
2022-08-18 19:34:59 +0300

    add basic css to product card

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
-import './ProductListItem.css'
+// import './ProductListItem.css'
 import './ProductListItem.scss'
 
 type PropsBlaBla = {
@@ -21,14 +21,14 @@ const ProductListItem = ({
     return (
         <Card>
             <CardContent>
-                <h4 className="product-title">{name}</h4>
+                <h3 className="product-title">{name}</h3>
                 <div className="product-description">{description}</div>
                 <div className="product-features red">Type: {type}</div>
                 <div className="product-features green">{capacity} Gb</div>
                 <div className="product-price">$ {price}</div>
             </CardContent>
 
-            <CardActions>
+            <CardActions className="btn-wrap">
                 <Button variant="outlined">Add to cart</Button>
             </CardActions>
         </Card>
