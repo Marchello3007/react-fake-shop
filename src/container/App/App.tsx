129230c6759d5379d5c3665eb0bc9eed221fd5cf
Oleg <marchello3007@gmail.com>
2022-09-02 07:37:19 +0300

    no result DZ

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -9,18 +9,27 @@ type ProductsInCartProps = {
 
 const App = () => {
     const [productsInCart, setProductsInCart] = useState<ProductsInCartProps>({
-        1: 1,
-        2: 1,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
     })
 
-    const addProductToCart = () => {
-        console.log('test')
+    const addProductToCart = (id: number, count: number) => {
+        setProductsInCart((prevState: ProductsInCartProps) => ({
+            [id]: prevState[id] + count,
+        }))
+
+        console.log(id)
+        console.log(count)
     }
 
     return (
         <>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
+            <button onClick={() => addProductToCart(1, 2)}>Add To Cart</button>
             <Main addProductToCart={addProductToCart} />
         </>
     )
