95858daef864dfabd5e20f6600841d07cf5aa3bb
Oleg <marchello3007@gmail.com>
2022-09-03 13:37:15 +0300

    add React Router and Route to cart

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Blabla from 'container/App/App'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<Blabla />)
+root.render(
+    <BrowserRouter>
+        <Blabla />
+    </BrowserRouter>
+)
