25478f0987d9494b740773f5d37b030f353a82fd
Oleg <marchello3007@gmail.com>
2022-09-03 14:29:58 +0300

    add CartPage

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { keys } from 'lodash'
+import productsArray, {
+    getProductObject,
+    Product,
+} from 'components/Products/productsArray'
 
-type Props = {}
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
 
-const CartPage = (props: Props) => {
-    return <div>CartPage</div>
+const CartPage = ({
+    productsInCart,
+    productsObject = getProductObject(productsArray),
+}: Props) => {
+    return (
+        <div>
+            {keys(productsInCart).map((productId) => (
+                <div key={productId}>
+                    {productsObject[parseInt(productId)].name}:{' '}
+                    {productsInCart[parseInt(productId)]} :{' '}
+                    {productsObject[parseInt(productId)].price}
+                </div>
+            ))}
+            <div>
+                Total{' '}
+                {keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsObject[parseInt(productId)].price *
+                            productsInCart[parseInt(productId)],
+                    0
+                )}
+                $
+            </div>
+        </div>
+    )
 }
 
 export default CartPage
