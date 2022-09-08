f81de201d55cafe33bd9f0d0cd66013468cde9bb
Oleg <marchello3007@gmail.com>
2022-09-08 08:22:57 +0300

    add removeProductFromCart with js

--- src/components/Cart/CartProductListItemExtended.tsx
+++ src/components/Cart/CartProductListItemExtended.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent, Grid } from '@mui/material'
+import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'components/Products/productsArray'
+import DeleteIcon from '@mui/icons-material/Delete'
 import React from 'react'
 
 type Props = {
@@ -22,6 +23,9 @@ const CartProductListItemExtended = ({ productCount, product }: Props) => {
                     <div>{product.name}</div>
                     <p>Price for one item: {product.price}</p>
                     <p>Count: {productCount}</p>
+                    <Button variant="outlined">
+                        <DeleteIcon />
+                    </Button>
                 </CardContent>
             </Card>
         </Grid>
